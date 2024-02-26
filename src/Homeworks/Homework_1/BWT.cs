f393aea612dd6d876e83d82785a2299ff46582a3
ira.sazonova.spb@gmail.com <ira.sazonova.spb@gmail.com>
2024-02-26 20:59:17 +0300

    Add: tests (SuffmassCreate, TransformBW, StringIndex, ReverseBW), Handleempty line exception

---
+++ src/Homeworks/Homework_1/BWT.cs
@@ -0,0 +1,73 @@
+namespace BWT 
+{
+    public static class BWTransform
+    {
+        private static bool LineVerification(string InputString)
+        {
+            if (string.IsNullOrEmpty(InputString))
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public static string[] SuffmasCreate(string InputString)
+        {
+            string [] Permutations = new string [InputString.Length];
+            Permutations[0] = InputString;
+            
+            for (int i = 1; i < Permutations.Length; ++i)
+            {
+                Permutations[i] = Permutations[i - 1].Substring(1) + Permutations[i - 1][0];
+            }
+
+            Array.Sort(Permutations);
+
+            return Permutations;
+        }
+
+        public static string TransformBW(string InputString)
+        {
+            if (!LineVerification(InputString))
+            {
+                Console.WriteLine("Input string can not be empty");
+                System.Environment.Exit(0);
+            }
+
+            string [] Permutations = SuffmasCreate(InputString);
+            string TransformString = string.Empty;
+
+            foreach (var element in Permutations)
+            {
+                TransformString = TransformString + element[Permutations.Length - 1];
+            }
+
+            return TransformString;
+        }
+        public static int SearchIndex(string InputString)
+        {
+            string [] Permutations = new string [InputString.Length];
+            Permutations = SuffmasCreate(InputString);
+
+            return Array.BinarySearch(Permutations, InputString) + 1;
+        }
+
+        public static string ReverseTransformBW(string OutputString, int pos)
+        {
+            string[] StringTable = new string[OutputString.Length];
+
+            for (int j = 0; j < OutputString.Length; ++j)
+            {
+                for (int i = 0; i < OutputString.Length; ++i)
+                {
+                    StringTable[i] = OutputString[i] + StringTable[i];
+                }
+
+                Array.Sort(StringTable);
+            }
+
+            return StringTable[pos - 1];
+        }
+    }
+}
---
+++ src/Homeworks/Homework_1/Program.cs
@@ -0,0 +1,118 @@
+﻿using BWT;
+ 
+bool TestSuffmassCreate()
+{
+    string[] InputString = new string[] {"abc","ABACABA", "apple"};
+    string[][] Expected = new string[][] 
+    {
+    new string[] {"abc", "bca", "cab"}, 
+    new string[] {"AABACAB","ABAABAC", "ABACABA", "ACABAAB", "BAABACA", "BACABAA", "CABAABA"},
+    new string[] {"apple", "eappl","leapp","pleap","pplea"} 
+    };
+    bool Passed = true;
+    for (int i = 0; i < InputString.Length; ++i) 
+    {
+        string[] Actual = BWTransform.SuffmasCreate(InputString[i]);
+        
+        if (!Enumerable.SequenceEqual(Actual, Expected[i]))
+        {
+            Passed = false;
+        }
+    }
+
+    return Passed;
+}
+
+bool TestTransformBW()
+{
+    string [] InputString = new string [] {"ABACABA","apple","colobok23"};
+    string[] Expected = new string [] {"BCABAAA","elppa","k2o3oolbc"};
+    bool Passed = true;
+    for (int i = 0; i < InputString.Length; ++i) 
+    {
+        string Actual = BWTransform.TransformBW(InputString[i]);
+        
+        if (!(Actual == Expected[i]))
+        {
+            Passed = false;
+        }
+    }
+
+    return Passed;
+}
+
+bool TestSearchIndex()
+{
+    string[] InputString = new string [] {"ABACABA","apple","colobok23"};
+    int [] Expected = new int [] {3, 1, 4};
+    bool Passed = true;
+    for (int i = 0; i < 3; ++i)
+    {
+        int Actual = BWTransform.SearchIndex(InputString[i]);
+
+        if (!(Actual == Expected[i]))
+        {
+            Passed = false;
+        }
+    }
+
+    return Passed;
+}
+
+bool TestReverseTransformBW()
+{
+    string [] OutputString = new string [] {"BCABAAA","elppa","k2o3oolbc"};
+    int [] StringIndex = new int [] {3, 1, 4};
+    string [] Expected = new string [] {"ABACABA","apple","colobok23"};
+    bool Passed = true;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        string Actual = BWTransform.ReverseTransformBW(OutputString[i], StringIndex[i]);
+
+        if (!(Actual == Expected[i]))
+        {
+            Passed = false;
+        }
+    }
+
+    return Passed;
+}
+
+bool Test()
+{
+    var TestCases = new bool[] {TestSuffmassCreate(), TestTransformBW(), TestSearchIndex(), TestReverseTransformBW()};
+    for (int i = 0; i < TestCases.Length; ++i)
+    {
+        if (!TestCases[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void UserInterface()
+{
+    Console.Write("Input the string: ");
+    string? InputString = Console.ReadLine();
+    
+    string TransformString = BWTransform.TransformBW(InputString);
+    int StringIndex = BWTransform.SearchIndex(InputString);
+
+    Console.WriteLine("Transform string: {0}, {1} ", TransformString, StringIndex);
+
+    Console.WriteLine("Reverse Burrows-Wheeler transform: {0}", BWTransform.ReverseTransformBW(TransformString, StringIndex));
+}
+
+Console.WriteLine("Initiation tests");
+
+if (Test())
+{
+    UserInterface();
+}
+else 
+{
+    Console.Write("Test failed");
+}
